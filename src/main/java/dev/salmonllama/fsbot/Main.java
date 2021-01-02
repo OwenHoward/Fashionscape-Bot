437512b0b9d1e60c6059aeb37706ef6f84187897
Salmonllama <a.gryczewski999@gmail.com>
2021-01-02 00:14:50 -0500

    Added support for MessageAttachments in reports

--- src/main/java/dev/salmonllama/fsbot/Main.java
+++ src/main/java/dev/salmonllama/fsbot/Main.java
@@ -17,8 +17,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class Main {
 
